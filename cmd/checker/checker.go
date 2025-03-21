3d038ed344e9a15bf6298e50facfcb5389c18ba7
helouazizi <ouazizi2code@gmail.com>
2025-03-21 00:36:29 +0000

    add scanner

--- cmd/checker/checker.go
+++ cmd/checker/checker.go
@@ -1,5 +1,19 @@
 package main
 
-func main()  {
-	println("checker")
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		println()
+		return
+	}
+	
+	stack := os.Args[1]
+	NewScanner := bufio.NewScanner(os.Stdin)
+	NewScanner.Scan()
+	instarctions := NewScanner.Text()
+	println(stack, instarctions)
 }
