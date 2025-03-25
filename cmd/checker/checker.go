1c1e45ccf48a4e5bd1412c753c99a8db975c18db
helouazizi <ouazizi2code@gmail.com>
2025-03-25 15:20:16 +0000

    add previous

--- cmd/checker/checker.go
+++ cmd/checker/checker.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"push-swap/internal/stacks"
@@ -9,104 +8,30 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 0 {
+	if len(os.Args) <= 1 {
+		println()
 		return
 	}
 
-	// Parse initial stack
-	stackA := utils.ParseArgs(args)
-	if stackA == nil {
-		fmt.Fprintln(os.Stderr, "Error")
-		os.Exit(1)
+	stack := os.Args[1]
+	instarctions, err := utils.Scsn_Input()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	stackB := stacks.New_stacks()
-
-	// Read and execute operations
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		op := Operation(scanner.Text())
-
-		// Validate operation
-		validOp := false
-		for _, knownOp := range []Operation{PA, PB, SA, SB, SS, RA, RB, RR, RRA, RRB, RRR} {
-			if op == knownOp {
-				validOp = true
-				break
-			}
-		}
-
-		if !validOp || !executeOperation(stackA, stackB, op) {
-			fmt.Fprintln(os.Stderr, "Error")
-			os.Exit(1)
-		}
+	all_stacks, err := utils.Parse_stack(stack)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	// Check final state
-	if len(stackB.items) == 0 && isSorted(stackA) {
-		fmt.Println("OK")
-	} else {
-		fmt.Println("KO")
+	fmt.Println(all_stacks.Stack_A, all_stacks.Stack_B, instarctions)
+	// lets execute the instarcions on the stacks
+	if len(stacks.Instarctions) != 0 {
+		all_stacks.Execute_Instarcrions(instarctions)
 	}
-}
+	//now if the stack a is sorted  and the stack b is empty 
+	// display ok folowed by \n
+	// otherwise display ko
+	fmt.Println("all instrctons are completed")
 
-func executeOperation(stackA, stackB *Stack, op Operation) bool {
-	switch op {
-	case PA:
-		val, ok := stackB.Pop()
-		if !ok {
-			return false
-		}
-		stackA.Push(val)
-
-	case PB:
-		val, ok := stackA.Pop()
-		if !ok {
-			return false
-		}
-		stackB.Push(val)
-
-	case SA:
-		stackA.Swap()
-
-	case SB:
-		stackB.Swap()
-
-	case SS:
-		stackA.Swap()
-		stackB.Swap()
-
-	case RA:
-		stackA.Rotate()
-
-	case RB:
-		stackB.Rotate()
-
-	case RR:
-		stackA.Rotate()
-		stackB.Rotate()
-
-	case RRA:
-		stackA.ReverseRotate()
-
-	case RRB:
-		stackB.ReverseRotate()
-
-	case RRR:
-		stackA.ReverseRotate()
-		stackB.ReverseRotate()
-	}
-
-	return true
-}
-
-func isSorted(stack *Stack) bool {
-	items := stack.items
-	for i := 1; i < len(items); i++ {
-		if items[i-1] > items[i] {
-			return false
-		}
-	}
-	return true
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,53 +1,58 @@
 package utils
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"push-swap/internal/stacks"
+	"slices"
 	"strconv"
+	"strings"
 )
 
-// func IsExist(arr []string, elm string) bool {
-// 	return slices.Contains(arr, elm)
-// }
+func IsExist(arr []string, elm string) bool {
+	return slices.Contains(arr, elm)
+}
 
-// func Scsn_Input() ([]string, error) {
-// 	instarctions := []string{}
-// 	NewScanner := bufio.NewScanner(os.Stdin)
-// 	if len(NewScanner.Bytes()) == 0 {
-// 		return nil, nil
-// 	}
-// 	for NewScanner.Scan() {
-// 		if NewScanner.Text() != "" && !IsExist(stacks.Instarctions, strings.TrimSpace(NewScanner.Text())) {
-// 			//println("Error: instruction do not exist :", NewScanner.Text())
-// 			return nil, fmt.Errorf("error: instruction do not exist :%s", NewScanner.Text())
-// 		}
-// 		// lets check for
-// 		if NewScanner.Text() != "" {
-// 			instarctions = append(instarctions, strings.TrimSpace(NewScanner.Text()))
-// 		}
+func Scsn_Input() ([]string, error) {
+	instarctions := []string{}
+	NewScanner := bufio.NewScanner(os.Stdin)
+	if len(NewScanner.Bytes()) == 0 {
+		return nil, nil
+	}
+	for NewScanner.Scan() {
+		if NewScanner.Text() != "" && !IsExist(stacks.Instarctions, strings.TrimSpace(NewScanner.Text())) {
+			//println("Error: instruction do not exist :", NewScanner.Text())
+			return nil, fmt.Errorf("error: instruction do not exist :%s", NewScanner.Text())
+		}
+		// lets check for
+		if NewScanner.Text() != "" {
+			instarctions = append(instarctions, strings.TrimSpace(NewScanner.Text()))
+		}
 
-// 	}
-// 	if err := NewScanner.Err(); err != nil {
-// 		return nil, err
-// 	}
+	}
+	if err := NewScanner.Err(); err != nil {
+		return nil, err
+	}
 
-// 	return instarctions, nil
-// }
+	return instarctions, nil
+}
 
-// func Parse_stack(text string) (*stacks.All_Stacks, error) {
-// 	stacks := stacks.New_stacks()
-// 	stck := strings.Split(strings.TrimSpace(text), " ")
-// 	slices.Reverse(stck)
-// 	for _, v := range stck {
-// 		num, err := strconv.Atoi(v)
-// 		if err != nil {
-// 			return nil, fmt.Errorf("error: invalid number '%v'", (v))
-// 		}
-// 		stacks.Stack_A = append(stacks.Stack_A, num)
-// 	}
-// 	return stacks, nil
-// }
+func Parse_stack(text string) (*stacks.All_Stacks, error) {
+	stacks := stacks.New_stacks()
+	stck := strings.Split(strings.TrimSpace(text), " ")
+	slices.Reverse(stck)
+	for _, v := range stck {
+		num, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, fmt.Errorf("error: invalid number '%v'", (v))
+		}
+		stacks.Stack_A = append(stacks.Stack_A, num)
+	}
+	return stacks, nil
+}
 
-func GetMax(stack *stacks.Stack) int {
+func GetMax(stack *stacks.All_Stacks) int {
 	num := stack.Stack_A[0]
 	for _, v := range stack.Stack_A {
 		if v >= num {
@@ -65,28 +70,7 @@ func BitsCount(num int) int {
 	}
 	return count
 }
-func ParseArgs(args []string) *stacks.Stack {
-	stack := stacks.New_stacks()
 
-	// Parse arguments into integers
-	for i := len(args) - 1; i >= 0; i-- {
-		num, err := strconv.Atoi(args[i])
-		if err != nil || contains(stack.Items, num) {
-			return nil
-		}
-		stack.Push(num)
-	}
-
-	return stack
-}
-func contains(items []int, target int) bool {
-	for _, item := range items {
-		if item == target {
-			return true
-		}
-	}
-	return false
-}
 func Abs(num int) int {
 	if num < 0 {
 		return -num
