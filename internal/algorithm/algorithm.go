50027b91235f4e6d9487c39f16683e06c95752cb
helouazizi <ouazizi2code@gmail.com>
2025-03-27 15:52:05 +0000

    remove some code

--- internal/algorithm/algorithm.go
+++ internal/algorithm/algorithm.go
@@ -12,15 +12,15 @@ func SortStack(a, b *stack.Stack) []string {
 
 	// Simple sorting logic (placeholder for an optimal algorithm)
 	for len(a.Items) > 0 {
-		minIdx := findMaxIndex(a)  
-		fmt.Println(minIdx,a.Items[minIdx])          // Find the index of the minimum element
-		moveToTop(a, b, minIdx, &operations) // Move the minimum element to the top of stack a
-		stack.Push(a, b)                     // Push the minimum element to stack b
+		minIdx := findMaxIndex(a)
+		fmt.Println(minIdx, a.Items[minIdx], "index and it value") // Find the index of the minimum element
+		moveToTop(a, b, minIdx, &operations)                       // Move the minimum element to the top of stack a
+		stack.Push(a, b)                                           // Push the minimum element to stack b
 		operations = append(operations, "pb")
 
 		// Debugging: Print the current state of the stacks
 		// fmt.Println("After operation: pb")
-		
+
 		fmt.Println("Stack A:", a.Items)
 		fmt.Println("Stack B:", b.Items)
 	}
@@ -53,24 +53,25 @@ func findMaxIndex(s *stack.Stack) int {
 
 // moveToTop moves the smallest element to the top using the fewest rotations.
 func moveToTop(a, b *stack.Stack, index int, operations *[]string) {
+	fmt.Println(len(a.Items), "len of stack a")
 	if index >= len(a.Items)/2 {
 		// Rotate to move the smallest element to the top
 		for i := index; i < len(a.Items); i++ {
-			if index > len(a.Items) {
-				fmt.Println(true)
-			}
+			// if index > len(a.Items) {
+			// 	fmt.Println(true)
+			// }
 			a.Rotate()
 			*operations = append(*operations, "ra")
 		}
-		fmt.Println(a.Items,"inside moove up ra")
+		fmt.Println(a.Items, "inside moove up ra")
 	} else {
 		// Reverse rotate to move the smallest element to the top
 		fmt.Println("first")
-		for i := len(a.Items)-1 - index; i > 0; i-- {
+		for i := len(a.Items) - 1 - index; i > 0; i-- {
 			a.ReverseRotate()
-			fmt.Println(a.Items,"rra")
+			fmt.Println(a.Items, "rra")
 			*operations = append(*operations, "rra")
 		}
-		fmt.Println(a.Items,"inside moove up rra")
+		fmt.Println(a.Items, "inside moove up rra")
 	}
 }
